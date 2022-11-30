9ad7eadb929dc7d3ba8e5edfb59f01717f01cbba
Maciej Białas <maciej.bialas@heycloud.pl>
2022-11-30 01:10:26 +0100

    mars weather api with up to date data

--- src/services/marsWeatherApi.tsx
+++ src/services/marsWeatherApi.tsx
@@ -1,9 +1,9 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
+//https://mars.nasa.gov/rss/api/?feed=weather&category=msl&feedtype=json
 // Define a service using a base URL and expected endpoints
 export const marsWeatherApi = createApi({
   reducerPath: "marsWeatherApi",
-  baseQuery: fetchBaseQuery({ baseUrl: `https://mars.nasa.gov/rss/api/?feed=weather&category=insight_temperature&feedtype=json&ver=1.0` }),
+  baseQuery: fetchBaseQuery({ baseUrl: `https://mars.nasa.gov/rss/api/?feed=weather&category=${'msl'}&feedtype=json&ver=1.0` }),//category - weather station name
   endpoints: (builder) => ({
     getData: builder.query({
       query: () => ``,
