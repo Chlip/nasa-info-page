c23d337b859d1f2637a7320123dd71e9b95c199f
Maciej Białas <maciej.bialas@heycloud.pl>
2022-12-27 17:17:07 +0100

    correct satelite display

--- src/pages/SatelitesPage.tsx
+++ src/pages/SatelitesPage.tsx
@@ -18,17 +18,27 @@ const SatelitesPage: React.FunctionComponent<ISatelitesPage> = () => {
   ${el.line1}
   ${el.line2}`)
   console.log(sat)
-  const latLonObj = !isLoading && !isFetching && getLatLngObj(sat[0]);
-  console.log(latLonObj)
-  !isLoading && !isFetching && getGroundTracks({
-    tle: sat[3],
-    startTimeMS: 1502342329860,
-    stepMS: 1000,
-    isLngLatFormat: true,
-}).then((res:any)=>{
-    console.log(res)
-});
+//   const latLonObj = !isLoading && !isFetching && getLatLngObj(sat[0]);
+//   console.log(latLonObj)
 
+  
+!isLoading && !isFetching && sat.forEach((s:any)=>{
+    try{
+        !isLoading && !isFetching && sat.length!=0 && getGroundTracks({
+            tle: s,
+            startTimeMS: 1502342329860,
+            stepMS: 1000,
+            isLngLatFormat: true,
+        }).then((res:any)=>{
+            console.log(`---------${s.split('\n')[0]}---------`)
+            console.log(res)
+            
+        });
+    }
+    catch{
+        console.log('err')
+    }
+})
   return (
     <Box sx={{ display: "flex", flexWrap: "wrap", justifyContent: "center",alignItems:"start" }}>
       satelites
