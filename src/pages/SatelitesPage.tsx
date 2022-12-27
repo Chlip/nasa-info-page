c4c5a277229b46c087d48572378d21e0f069b2f0
Maciej Białas <maciej.bialas@heycloud.pl>
2022-12-27 16:02:34 +0100

    satelite directions on map

--- src/pages/SatelitesPage.tsx
+++ src/pages/SatelitesPage.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 
 import CircularProgress from '@mui/material/CircularProgress';
 import { useGetCollectionQuery } from "../services/tleApi";
+import { getGroundTracks, getLatLngObj } from "tle.js";
 
 
 interface ISatelitesPage {}
@@ -11,9 +12,22 @@ const SatelitesPage: React.FunctionComponent<ISatelitesPage> = () => {
   
   const [startDate, setStartDate] = useState(new Date().toJSON().slice(0, 10));
   const [endDate, setEndDate] = useState(new Date().toJSON().slice(0, 10));
-  const { data, error, isLoading } = useGetCollectionQuery({ps:5, sort:"popularity" });
+  const { data, error, isLoading, isFetching } = useGetCollectionQuery({ps:5, sort:"popularity" });
   console.log(data);
-  
+  const sat = !isLoading && data.member.map((el:any) => `${el.name}
+  ${el.line1}
+  ${el.line2}`)
+  console.log(sat)
+  const latLonObj = !isLoading && !isFetching && getLatLngObj(sat[0]);
+  console.log(latLonObj)
+  !isLoading && !isFetching && getGroundTracks({
+    tle: sat[3],
+    startTimeMS: 1502342329860,
+    stepMS: 1000,
+    isLngLatFormat: true,
+}).then((res:any)=>{
+    console.log(res)
+});
 
   return (
     <Box sx={{ display: "flex", flexWrap: "wrap", justifyContent: "center",alignItems:"start" }}>
